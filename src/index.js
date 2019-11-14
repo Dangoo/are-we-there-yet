222036b4bbf22ad594867cd12576b9ae1d28f1cd
dangoo <daniel.gooss@sinnerschrader.com>
2019-11-14 10:32:50 +0100

    feat: optimizations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import FlipClock from "flipclock";
 
 const el = document.querySelector('.js-clock');
+const DATE = new Date('2019-11-15T12:00:00+01:00');
 
-const clock = new FlipClock(el, new Date('2019-11-15T12:00:00+01:00'), {
+const clock = new FlipClock(el, DATE, {
     clockFace: 'DayCounter',
     countdown: true
 });
 
 clock.face = 'DayCounter'
+clock.timer.on('interval', () => { 
+    const stringTimer = clock.face.value.digits.map(digits => digits.join('')).join(':');
+
+    window.document.title = stringTimer;
+})
