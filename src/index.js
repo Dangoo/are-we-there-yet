6af847dd61171058b8a1f059c6540014acd76c88
dangoo <daniel.gooss@sinnerschrader.com>
2019-11-14 09:40:57 +0100

    feat: update timer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import FlipClock from "flipclock";
 
 const el = document.querySelector('.js-clock');
 
-const clock = new FlipClock(el, new Date('2019-11-15'), {
+const clock = new FlipClock(el, new Date('2019-11-15T12:00:00+01:00'), {
     clockFace: 'DayCounter',
     countdown: true
 });
